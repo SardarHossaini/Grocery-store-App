091e672f33c857f4200638920db5214a96b56cd9
Sardar219 <sardarhossaini2022@gmail.com>
2025-05-08 11:02:49 +0430

    setup project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(Grocery());
+}
+
+class Grocery extends StatefulWidget {
+  const Grocery({super.key});
+
+  @override
+  State<Grocery> createState() => _GroceryState();
+}
+
+class _GroceryState extends State<Grocery> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Grocery"),
+        ),
+      ),
+    );
+  }
+}
